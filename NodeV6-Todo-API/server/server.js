81c5ff7d8b548082f91c30781444bc9c07d29ee7
retr0spect <aditya.metalhead@gmail.com>
2016-12-02 18:57:23 -0700

    Added GET /todos:id

--- NodeV6-Todo-API/server/server.js
+++ NodeV6-Todo-API/server/server.js
@@ -5,9 +5,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -31,6 +33,21 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todos) => {
+        if (!todos) {
+            return res.send(404).send();
+        }
+        res.send({todos});
+    }).catch(() => {
+        res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
