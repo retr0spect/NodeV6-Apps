b69e5bab42607a8defc238e26b69657fec269a3a
Aditya Chandel <aditya.chandel@aggiemail.usu.edu>
2016-12-08 02:35:32 -0700

    Fixed mongoose duplicate key

--- NodeV6-Todo-API/server/server.js
+++ NodeV6-Todo-API/server/server.js
@@ -105,11 +105,9 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
-
     user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
-        console.log('Token: ' + token);
         res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
